90074864414c4a586677d9582383e0a85d907c56
n0noob <neemanoop@gmail.com>
2020-07-29 23:36:20 +0530

    Attempted

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useRef, useEffect } from 'react';
 import * as THREEJSHELPER from './helpers/threejs-helper';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import * as THREE from 'three';
 import './App.css';
 
@@ -8,10 +9,21 @@ function App() {
   const threeRenderer = useRef(null);
 
   useEffect(() => {
+    var loader = new GLTFLoader();
     const {scene, camera, renderer} = THREEJSHELPER.init();
     threeRenderer.current.appendChild( renderer.domElement );
     let cube = THREEJSHELPER.getCube();
     let light = THREEJSHELPER.getLight();
+    cube.position.x = 3;
+    loader.load( './glb/monkey.glb', ( gltf ) => {
+
+      scene.add( gltf.scene );
+    
+    }, undefined, ( error ) => {
+    
+      console.error( error );
+    
+    });
     scene.add( cube );
     scene.add(light);
     (function animate() {
